30ae3159d74375f9311a3a292d50e54b1714d1c7
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-11-03 10:37:58 -0500

    First commit

---
+++ src/Hello.java
@@ -0,0 +1,18 @@
+public class Hello {
+    public static void main(String[] args) {
+
+
+//        public static int calculateHighScorePosition(int playerScore) {
+//            if (playerScore > 1000)
+//            {
+//                "The score is greater than a 1000";
+//            }
+//            return
+//        }
+        displayHighScorePosition("Ken", 1);
+    }
+    public static void displayHighScorePosition(String playerName, int position) {
+        System.out.println(playerName + " managed to get position " + position);
+    }
+
+}
