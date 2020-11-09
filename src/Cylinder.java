5bf9d50ef71d66045ba94c84b2644953641655ff
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-11-09 11:39:22 -0500

    Made shapes for OOP

---
+++ src/Cylinder.java
@@ -0,0 +1,19 @@
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder(double height, double radius) {
+        super(radius);
+        if (height < 0) {
+            this.height = 0;
+            return;
+        }
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+    public double getVolume() {
+        return getArea() * height;
+    }
+}
