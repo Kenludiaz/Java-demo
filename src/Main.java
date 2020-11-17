5a2b481b231d420b8a767275966689e73eb6b219
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-11-17 11:21:09 -0500

    Studying Generics.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        //< T extends animal> gets rid of the need to cast from using <T>
+        // Parameterized typing
     }
 
 }
