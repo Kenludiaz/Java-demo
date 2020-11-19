99d681b910f5378c4f9ab9db9d81a7fced8cb4d6
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-11-19 09:32:24 -0500

    Working with packages.

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,3 @@
-
-public class Main {
-    public static void main(String[] args) {
-        League<SoccerTeam> SoccerLeague = new League<>();
-        SoccerTeam Mudslingers = new SoccerTeam("Mudslingers");
-        SoccerTeam Netters = new SoccerTeam("Netters");
-        SoccerTeam Brazilers = new SoccerTeam("Brazilers");
-        BasketballTeam Hoopers = new BasketballTeam("Hoopers");
-
-        SoccerLeague.addTeam(Mudslingers);
-        SoccerLeague.addTeam(Netters);
-        SoccerLeague.addTeam(Brazilers);
-//        SoccerLeague.addTeam(Hoopers);
-//        SoccerLeague.addTeam("Hello");
-        Mudslingers.wonAgainst(Hoopers);
-        Mudslingers.wonAgainst(Netters);
-        Netters.wonAgainst(Mudslingers);
-        Mudslingers.tiedAgainst(Netters);
-
-        SoccerLeague.showLeagueTable();
-    }
+public class Main{
 
 }
---
+++ src/Series.java
@@ -0,0 +1,18 @@
+public class Series {
+    static int nSUm(int n) {
+        int result = 0;
+        for (int i = 1; i < n; i++) {
+            result += i;
+        }
+        return result;
+    }
+
+    static int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i < n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+    static int fibonacci
+}
