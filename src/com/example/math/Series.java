59ab0b0aceab7d74569f23169858686846bdbb5d
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-11-20 10:13:30 -0500

    Made a package.

--- src/com/example/math/Series.java
+++ src/com/example/math/Series.java
@@ -1,3 +1,5 @@
+package com.example.math;
+
 public class Series {
     static int nSUm(int n) {
         int result = 0;
@@ -14,5 +16,14 @@ public class Series {
         }
         return result;
     }
-    static int fibonacci
+    static int fibonacci(int n) {
+        int prev = 0;
+        int current = 1;
+        for (int i = 1; i < n; i++) {
+            int temp = current;
+            current = current + prev;
+            prev = temp;
+        }
+        return current;
+    }
 }
