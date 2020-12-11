f0f7b5b0ff3fd30a936999e00b34e697af6a5b46
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-12-11 14:13:38 -0500

    Implements reentrant and try lock

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,21 +1,48 @@
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
     private double balance;
     private String accountNumber;
 
+    private Lock lock;
+
     public BankAccount(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
+        this.lock = new ReentrantLock();
     }
 
     public void deposit(double amount) {
-        synchronized (this) {
-            balance += amount;
-        }
+        try {
+            if (lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance += amount;
+                } finally {
+                    lock.unlock();
+                }
+            }
+
+        } catch (InterruptedException e) {
+
+            }
+
     }
 
+
     public void withdraw(double amount) {
-        synchronized (this) {
-            balance -= amount;
+        try {
+            if (lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance -= amount;
+                } finally {
+                    lock.unlock();
+                }
+
+            }
+        } catch (InterruptedException e) {
+
         }
     }
 
