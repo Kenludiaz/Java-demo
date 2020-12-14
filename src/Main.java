a3e312d3a2d90e03b40c5d94fa4bc248c509893b
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-12-14 13:48:27 -0500

    Finishes the first page of challenges

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,6 @@ import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-        Function<String, String> lambda = (String source) -> {
-        StringBuilder returnVal = new StringBuilder();
-        for (int i = 0; i < ((String) source).length(); i++) {
-            if (i % 2 == 1) {
-                returnVal.append(source.charAt(i));
-            }
-        }
-        return returnVal.toString();
-    };
 //        Runnable runnable =
 //        () -> {
 //            String myString = "Let's Split this up into an array";
@@ -20,7 +11,24 @@ public class Main {
 //            }
 //
 //        };
-        String result = lambda.apply("1234567890");
-        System.out.println(result);
+//        Function<String, String> lambda = (String source) -> {
+//        StringBuilder returnVal = new StringBuilder();
+//        for (int i = 0; i < ((String) source).length(); i++) {
+//            if (i % 2 == 1) {
+//                returnVal.append(source.charAt(i));
+//            }
+//        }
+//        return returnVal.toString();
+//        };
+//        Supplier<String> s = () -> "I love Java!";
+//        String supplierResult = s.get();
+//
+//        System.out.println(supplierResult);
+
+
+    }
+
+    public static String everySecond(Function<String, String> func, String source) {
+        return func.apply(source);
     }
 }
