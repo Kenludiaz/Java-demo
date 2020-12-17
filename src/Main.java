1b6151e5fc4f67d2903fd1c6ae621b4b4e833af0
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-12-17 10:26:08 -0500

    Adds regex challenges

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,50 @@
-import java.util.Arrays;
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-//        Runnable runnable =
-//        () -> {
-//            String myString = "Let's Split this up into an array";
-//            String[] parts = myString.split(" ");
-//            for (String part : parts) {
-//                System.out.println(part);
-//            }
-//
-//        };
-//        Function<String, String> lambda = (String source) -> {
-//        StringBuilder returnVal = new StringBuilder();
-//        for (int i = 0; i < ((String) source).length(); i++) {
-//            if (i % 2 == 1) {
-//                returnVal.append(source.charAt(i));
-//            }
-//        }
-//        return returnVal.toString();
-//        };
-//        Supplier<String> s = () -> "I love Java!";
-//        String supplierResult = s.get();
-//
-//        System.out.println(supplierResult);
 
-    List<String> topNames = Arrays.asList(
-            "Amelia",
-            "Olivia",
-            "emily",
-            "Isla",
-            "Ava",
-            "Oliver",
-            "Jack",
-            "Charlie",
-            "harry",
-            "Javob"
-    );
-        topNames.stream()
-                .map(s -> {
-                    String upper = (s.substring(0, 1)).toUpperCase();
-                    return upper + s.substring(1);
-                })
-                .peek(System.out::println)
-                .sorted(String::compareTo)
-                .forEach(System.out::println);
+        String challenge1 = "I want a bike.";
+        String challenge2 = "I want a ball.";
+
+        String regex = "I want a b.+";
+        Pattern regexPattern = Pattern.compile("I want a b.+");
+        Matcher matcher = regexPattern.matcher(challenge1);
+        Matcher matcher2 = regexPattern.matcher(challenge1);
+
+//        Matcher answer = reg
+
+        System.out.println(matcher.matches());
+        System.out.println(matcher2.matches());
+
+        String challenge4 = "Replace all blanks with underscores.";
+        System.out.println(challenge4.replaceAll(" ", "_"));
+
+        String challenge5 = "aaabccccccddddeffffg";
+        System.out.println(challenge5.matches("[a-g]+"));
+
+        String challenge7 = "abcd.125avdcsdf.7tstzfa.999";
+        System.out.println(challenge7.matches("\\w+.\\d+"));
+
+        Pattern pattern = Pattern.compile("(\\d+)");
+        matcher = pattern.matcher(challenge7);
+        System.out.println("The numbers in the pattern are:");
+        while (matcher.find()) {
+            System.out.println(matcher.start(1));
+        }
+
+
+        String challenge = "{0,2}, {1,5}, {4,7}, {1,2},{2,4}";
+        pattern = Pattern.compile("\\{(.+?)}");
+        matcher = pattern.matcher(challenge);
+        System.out.println("The coordinates are: ");
+        while (matcher.find()) {
+            System.out.println(matcher.group(1));
+        }
+
+        challenge = "11111-1234";
+        pattern = Pattern.compile("(^\\d{5}-\\d{4})?$");
+        matcher = pattern.matcher(challenge);
+        System.out.println(matcher.matches());
     }
-//
-//    public static String everySecond(Function<String, String> func, String source) {
-//        return func.apply(source);
-//    }
 }
