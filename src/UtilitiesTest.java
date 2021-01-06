e5bbda293d61dabcb3f98679496b306816df30c2
Kenneth Lu Diaz <ludiazken@gmail.com>
2021-01-06 10:24:47 -0500

    Adds test for exception thrown in converter

--- src/UtilitiesTest.java
+++ src/UtilitiesTest.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.Before;
 
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertNull;
@@ -6,17 +7,23 @@ import static junit.framework.TestCase.assertEquals;
 
 public class UtilitiesTest {
 
-    Utilities utilities = new Utilities();
+    private Utilities utilities;
+
+    @Before
+    public void setup() {
+        utilities = new Utilities();
+    }
 
     @org.junit.Test
     public void testEveryNthChar() throws Exception {
-        char [] expected = {'e','l'};
-        Assert.assertArrayEquals(expected,  utilities.everyNthChar(new char[]{'h', 'e', 'l', 'l', 'o'}, 2));
+        char[] expected = {'e', 'l'};
+        Assert.assertArrayEquals(expected, utilities.everyNthChar(new char[]{'h', 'e', 'l', 'l', 'o'}, 2));
 
     }
+
     @org.junit.Test
     public void testEveryNthCharBigN() throws Exception {
-        Assert.assertArrayEquals(new char[] {'h', 'o', 'h'}, utilities.everyNthChar(new char[] {'h', 'o', 'h'}, 5));
+        Assert.assertArrayEquals(new char[]{'h', 'o', 'h'}, utilities.everyNthChar(new char[]{'h', 'o', 'h'}, 5));
     }
 
     @org.junit.Test
@@ -32,7 +39,13 @@ public class UtilitiesTest {
         assertEquals(300, utilities.converter(10, 5));
     }
 
+    @org.junit.Test(expected = ArithmeticException.class)
+    public void testConverterException() throws Exception {
+        utilities.converter(10, 0);
+    }
+
     @org.junit.Test
+
     public void testNullIfOddLength() throws Exception {
         assertNull(utilities.nullIfOddLength("hello"));
         assertNotNull("hell");
