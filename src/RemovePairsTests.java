035e83980ffacc42ca058b8c660f197f46a14388
Kenneth Lu Diaz <ludiazken@gmail.com>
2021-01-07 11:38:00 -0500

    Adds Run With rule

--- src/RemovePairsTests.java
+++ src/RemovePairsTests.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class RemovePairsTests {
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+
+public class RemovePairsTests {
+
+    private Utilities utilities;
+
+    private String input;
+    private String output;
+
+    public RemovePairsTests(String input, String output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public static Collection<Object> testConditions() {
+            return Arrays.asList(new Object [][]
+            {{"ABCDEFF", "ABCDEF"},
+            {"AB88EFFG", "AB8EFG"},
+            {"112233445566", "123456"},
+            {"ZYZQQB", "ZYZQB"},
+            {"A", "A"}
+            });
+
+    }
+
+    @org.junit.Before
+    public void setup() {
+        utilities = new Utilities();
+    }
+
+    @org.junit.Test
+    public void testRemovePairsArray() throws Exception {
+        assertEquals()
+    }
+
 }
